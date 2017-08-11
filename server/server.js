81ddf8784f300b6a621fd829ad4a04e3880fa830
Qasim Laghari <go.jets.laghari@gmail.com>
2017-08-10 22:07:46 -0500

    Update GraphQL uri

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb://ladmin:test@ds027415.mlab.com:27415/lyricaldb';
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
