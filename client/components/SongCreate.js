c237213876608efb0d2c4a669dd5c40382ca6c15
Qasim Laghari <go.jets.laghari@gmail.com>
2017-09-06 15:09:40 -0500

    Update component ease of use

--- client/components/SongCreate.js
+++ client/components/SongCreate.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
+import { Link, hashHistory } from 'react-router';
 
 class SongCreate extends Component {
 
@@ -19,13 +20,14 @@ class SongCreate extends Component {
       variables: {
         title: this.state.title
       }
-    });
+    }).then( () => hashHistory.push('/'))
 
 }
 
   render() {
     return (
       <div>
+        <Link to="/">Back</Link>
         <h3>Create a new song, hommie!</h3>
         <form onSubmit={this.onSubmit.bind(this)} >
           <label>Song Title</label>
