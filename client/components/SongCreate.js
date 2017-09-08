e4867644624f165c43a6c80e7e63cdd1a58b5354
Qasim Laghari <go.jets.laghari@gmail.com>
2017-09-07 19:57:41 -0500

    Abstract fetch query

--- client/components/SongCreate.js
+++ client/components/SongCreate.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
 import { Link, hashHistory } from 'react-router';
+import query from '../queries/fetchSongs';
 
 class SongCreate extends Component {
 
@@ -17,9 +18,8 @@ class SongCreate extends Component {
     console.log(this.props);
 
     this.props.mutate({
-      variables: {
-        title: this.state.title
-      }
+      variables: { title: this.state.title },
+      refetchQueries: [{ query }]
     }).then( () => hashHistory.push('/'))
 
 }
--- client/components/SongList.js
+++ client/components/SongList.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import gql from 'graphql-tag';
 import { graphql } from 'react-apollo';
 import { Link } from 'react-router';
+import query from '../queries/fetchSongs';
 
 class SongList extends Component {
 
@@ -37,13 +38,5 @@ class SongList extends Component {
   }
 }
 
-const query = gql `
-  {
-    songs {
-      id
-      title
-    }
-  }
-`;
 
 export default graphql(query)(SongList);
