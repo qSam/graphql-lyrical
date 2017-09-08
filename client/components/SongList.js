4cc3ba98ba8a0dd053f5bb5e3b7c88cbcecef554
Qasim Laghari <go.jets.laghari@gmail.com>
2017-09-07 20:46:27 -0500

    Refetch component data after delete

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -7,10 +7,8 @@ import query from '../queries/fetchSongs';
 class SongList extends Component {
 
   onSongDelete(id) {
-    this.props.mutate({
-      variables: {id:id}
-    });
-
+    this.props.mutate({variables: {id:id}})
+      .then( () => this.props.data.refetch());
   }
 
   renderSongs() {
